264eab8542fa562d7c4de968574ca895c25643dc
Jian <jian.wang@cpnet.io>
2022-12-19 08:54:43 +0800

    Add test cases for new options

--- kedro-airflow/tests/test_plugin.py
+++ kedro-airflow/tests/test_plugin.py
@@ -14,14 +14,26 @@ def identity(arg):
     "dag_name,pipeline_name,command",
     [
         # Test normal execution
-        ("hello_world_dag", "__default__", ["airflow", "create"]),
+        ("hello_world_default_dag", "__default__", ["airflow", "create"]),
         # Test execution with alternate pipeline name
-        ("hello_world_dag", "ds", ["airflow", "create", "--pipeline", "ds"]),
+        ("hello_world_ds_dag", "ds", ["airflow", "create", "--pipeline", "ds"]),
         # Test execution with different dir and filename for Jinja2 Template
         (
-            "hello_world_dag",
+            "hello_world_default_dag",
             "__default__",
             ["airflow", "create", "-j", "airflow_dag.j2"],
+        ),
+        # Test execution with the env variables option
+        (
+            "hello_world_default_dag",
+            "__default__",
+            ["airflow", "create", "-E", "var1=value1"],
+        ),
+        # Test execution with the
+        (
+            "hello_world_default_dag",
+            "__default__",
+            ["airflow", "create", "-w", "/opt/airflow/kedro/proj1"],
         ),
     ],
 )
@@ -44,6 +56,14 @@ def test_create_airflow_dag(
     assert dag_file.exists()
 
     expected_airflow_dag = 'tasks["node0"] >> tasks["node1"]'
+    expected_environment_settings = 'os.environ["var1"] = value1'
+    expected_working_dir_settings = "os.chdir('/opt/airflow/kedro/proj1')"
     with open(dag_file, "r", encoding="utf-8") as f:
         dag_code = [line.strip() for line in f.read().splitlines()]
     assert expected_airflow_dag in dag_code
+    if "-E" in command:
+        assert expected_environment_settings in dag_code
+    if "-w" in command:
+        assert expected_working_dir_settings in dag_code
+    else:
+        assert expected_working_dir_settings not in dag_code
